4212735c84745fe14f5d10e93fbee8e4aea035c5
SHAHIL <110395532+Shahilali5@users.noreply.github.com>
2025-02-06 09:51:00 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,111 @@
+import re
+import os
+import time
+
+id_pattern = re.compile(r'^.\d+$')
+
+
+class Config(object):
+    # pyro client config
+    API_ID = os.environ.get("API_ID", "Enter Api Id")  # ⚠️ Required
+    API_HASH = os.environ.get("API_HASH", "Enter Api Hash")  # ⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "Enter Your Bot Token")  # ⚠️ Required
+
+    # premium 4g renaming client
+    STRING_API_ID = os.environ.get("STRING_API_ID", "")
+    STRING_API_HASH = os.environ.get("STRING_API_HASH", "")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "")
+    
+    # database config
+    DB_NAME = os.environ.get("DB_NAME", "Snow_User_Data")
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://Filestorebot:Filestorebot@11@cluster0.dkey6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # ⚠️ Required
+
+    # other configs
+    BOT_UPTIME = time.time()
+    START_PIC = os.environ.get("START_PIC", "https://te.legra.ph/file/dd9e884aa2eb45d88155e-202896888e9a2d1c96.jpg")
+    ADMIN = [int(admin) if id_pattern.search(
+        admin) else admin for admin in os.environ.get('ADMIN', '8129810243').split()]  # ⚠️ Required
+    
+    FORCE_SUB = os.environ.get("FORCE_SUB", "haxkx2") # ⚠️ Required Username without @
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002276031775"))  # ⚠️ Required
+    FLOOD = int(os.environ.get("FLOOD", '10'))
+    BANNED_USERS = set(int(x) for x in os.environ.get(
+        "BANNED_USERS", "1234567890").split())
+
+    # wes response configuration
+    WEBHOOK = bool(os.environ.get("WEBHOOK", True))
+    PORT = int(os.environ.get("PORT", "8080"))
+
+
+class Txt(object):
+    # part of text configuration
+    START_TXT = """<b>Hɪ {} 👋,
+    
+Tʜɪs Is Aɴ Aᴅᴠᴀɴᴄᴇᴅ Aɴᴅ Yᴇᴛ Pᴏᴡᴇʀꜰᴜʟ Rᴇɴᴀᴍᴇ Bᴏᴛ
+Usɪɴɢ Tʜɪs Bᴏᴛ Yᴏᴜ Cᴀɴ Rᴇɴᴀᴍᴇ & Cʜᴀɴɢᴇ Tʜᴜᴍʙɴᴀɪʟ Oꜰ Yᴏᴜʀ Fɪʟᴇ
+Yᴏᴜ Cᴀɴ Aʟsᴏ Cᴏɴᴠᴇʀᴛ Vɪᴅᴇᴏ Tᴏ Fɪʟᴇ & Fɪʟᴇ Tᴏ Vɪᴅᴇᴏ
+Tʜɪs Bᴏᴛ Aʟꜱᴏ Sᴜᴘᴘᴏʀᴛs Cᴜsᴛᴏᴍ Tʜᴜᴍʙɴᴀɪʟ Aɴᴅ Cᴜsᴛᴏᴍ Cᴀᴘᴛɪᴏɴ
+"""
+
+    ABOUT_TXT = """<b>╭───────────⍟
+├🤖 Mʏ Nᴀᴍᴇ : {}
+├👨‍💻 Dᴇᴠᴇʟᴏᴘᴇʀ : <a href=https://t.me/Shahil440>ɪ֟፝ؖ۬ꪖꪑ • 𝛅ʜ⋏֟፝͝н𝛊ʟ</a>
+├👑 Iɴsᴛᴀɢʀᴀᴍ : <a href=https://www.instagram.com/Shahil2877>Iɴsᴛᴀɢʀᴀᴍ</a> 
+├☃️ ꜰᴏᴜɴᴅᴇʀ ᴏꜰ : <a href=https://t.me/Shahil440>𝛅ʜ⋏֟፝͝н𝛊ʟ</a>
+├📕 Lɪʙʀᴀʀy : <a href=https://github.com/pyrogram>Pyʀᴏɢʀᴀᴍ</a>
+├✏️ Lᴀɴɢᴜᴀɢᴇ: <a href=https://www.python.org>Pyᴛʜᴏɴ 3</a>
+├💾 Dᴀᴛᴀ Bᴀꜱᴇ: <a href=https://cloud.mongodb.com>Mᴏɴɢᴏ DB</a>
+╰───────────────⍟ """
+
+    HELP_TXT = """
+🌌 <b><u>Hᴏᴡ Tᴏ Sᴇᴛ Tʜᴜᴍʙɴɪʟᴇ</u></b>
+  
+<b>•></b> /start Tʜᴇ Bᴏᴛ Aɴᴅ Sᴇɴᴅ Aɴy Pʜᴏᴛᴏ Tᴏ Aᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟy Sᴇᴛ Tʜᴜᴍʙɴɪʟᴇ.
+<b>•></b> /del_thumb Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Dᴇʟᴇᴛᴇ Yᴏᴜʀ Oʟᴅ Tʜᴜᴍʙɴɪʟᴇ.
+<b>•></b> /view_thumb Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Vɪᴇᴡ Yᴏᴜʀ Cᴜʀʀᴇɴᴛ Tʜᴜᴍʙɴɪʟᴇ.
+
+
+📑 <b><u>Hᴏᴡ Tᴏ Sᴇᴛ Cᴜꜱᴛᴏᴍ Cᴀᴩᴛɪᴏɴ</u></b>
+
+<b>•></b> /set_caption - Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Sᴇᴛ ᴀ Cᴜꜱᴛᴏᴍ Cᴀᴩᴛɪᴏɴ
+<b>•></b> /see_caption - Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Vɪᴇᴡ Yᴏᴜʀ Cᴜꜱᴛᴏᴍ Cᴀᴩᴛɪᴏɴ
+<b>•></b> /del_caption - Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Dᴇʟᴇᴛᴇ Yᴏᴜʀ Cᴜꜱᴛᴏᴍ Cᴀᴩᴛɪᴏɴ
+Exᴀᴍᴩʟᴇ:- <code> /set_caption 📕 Fɪʟᴇ Nᴀᴍᴇ: {filename}
+💾 Sɪᴢᴇ: {filesize}
+⏰ Dᴜʀᴀᴛɪᴏɴ: {duration} </code>
+
+✏️ <b><u>Hᴏᴡ Tᴏ Rᴇɴᴀᴍᴇ A Fɪʟᴇ</u></b>
+<b>•></b> Sᴇɴᴅ Aɴy Fɪʟᴇ Aɴᴅ Tyᴩᴇ Nᴇᴡ Fɪʟᴇ Nɴᴀᴍᴇ \nAɴᴅ Aᴇʟᴇᴄᴛ Tʜᴇ Fᴏʀᴍᴀᴛ [ document, video, audio ].           
+
+
+<b>⦿ Dᴇᴠᴇʟᴏᴘᴇʀ:</b> <a href=https://t.me/Shahil440> ɪ֟፝ؖ۬ꪖꪑ • 𝛅ʜ⋏֟፝͝н𝛊ʟ 😎</a>
+"""
+    METADATA_TXT = """<b><u>🖼️  HOW TO SET CUSTOM METADATA</u></b>
+
+For Example :-
+
+<code>By @Shahil440</code>
+
+💬 For Any Help Contact @Shahil440
+"""
+
+    SEND_METADATA = """
+❪ SET CUSTOM METADATA ❫
+
+☞ Fᴏʀ Exᴀᴍᴘʟᴇ:-
+
+◦ <code> -map 0 -c:s copy -c:a copy -c:v copy -metadata title="Powered By:- @Shahil440" -metadata author="@Shahil440" -metadata:s:s title="Subtitled By :- @Shahil440" -metadata:s:a title=" @Shahil440" -metadata:s:v title=" @Shahil440" </code>
+
+📥 Fᴏʀ Hᴇʟᴘ Cᴏɴᴛ. @Shahil440
+"""
+
+    PROGRESS_BAR = """<b>\n
+╭━❰𝛅ʜ⋏֟፝͝н𝛊ʟ Rᴇɴᴀᴍɪɴɢ Rᴇᴘᴏʀᴛ❱━➣
+┣⪼ 🗃️ Sɪᴢᴇ: {1} | {2}
+┣⪼ ⏳️ Dᴏɴᴇ : {0}%
+┣⪼ 🚀 Sᴩᴇᴇᴅ: {3}/s
+┣⪼ 🔥 Bᴏᴛ Bʏ: @Shahil440
+╰━━━━━━━━━━━━━━━━━━━━➣
+
+<b>⦿ Dᴇᴠᴇʟᴏᴘᴇʀ:</b> @Shahil440
+</b>"""
