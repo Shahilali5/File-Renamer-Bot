3be56a925ab856450432225f6897b3a307bc9884
SHAHIL <110395532+Shahilali5@users.noreply.github.com>
2025-02-09 18:19:16 +0530

    Update Force_Sub.py

--- plugins/Force_Sub.py
+++ plugins/Force_Sub.py
@@ -1,4 +1,4 @@
-from pyrogram import Client, filters, enums 
+from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors import UserNotParticipant
 from config import Config
@@ -6,31 +6,34 @@ from helper.database import db
 
 async def not_subscribed(_, client, message):
     await db.add_user(client, message)
+    
     if not Config.FORCE_SUB:
-        return False
+        return False  # No force subscription required
+
     try:             
         user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id) 
         if user.status == enums.ChatMemberStatus.BANNED:
             return True 
-        else:
-            return False                
-    except UserNotParticipant:
-        pass
-    return True
+        return False  # User is not banned, so no restriction
+    except (UserNotParticipant, KeyError):  # Catch both exceptions
+        return True  # User is not a participant, so needs to subscribe
 
 
 @Client.on_message(filters.private & filters.create(not_subscribed))
 async def forces_sub(client, message):
-    buttons = [[InlineKeyboardButton(text=" 🔥 Uᴘᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ 🔥 ", url=f"https://t.me/{Config.FORCE_SUB}") ]]
-    text = "**Sᴏʀʀy Dᴜᴅᴇ, Yᴏᴜ Hᴀᴠᴇ Nᴏᴛ Jᴏɪɴᴇᴅ My Uᴩᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ 😐 \n Pʟᴇᴀꜱᴇ Jᴏɪɴ My Uᴩᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ Tᴏ Usᴇ Mᴇ **"
+    buttons = [
+        [InlineKeyboardButton(text="📢 ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/{Config.FORCE_SUB}")]
+    ]
+    
+    text = "**sᴏʀʀʏ, ʏᴏᴜ'ʀᴇ ɴᴏᴛ ᴊᴏɪɴᴇᴅ ᴍʏ ᴄʜᴀɴɴᴇʟ. ᴘʟᴇᴀsᴇ ᴊᴏɪɴ ᴏᴜʀ ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴄᴏɴᴛɪɴᴜᴇ.**"
+
     try:
         user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id)    
         if user.status == enums.ChatMemberStatus.BANNED:                                   
-            return await client.send_message(message.from_user.id, text="Sᴏʀʀy Yᴏᴜ'ʀᴇ Bᴀɴɴᴇᴅ Tᴏ Uꜱᴇ Mᴇ")  
-    except UserNotParticipant:                       
+            return await client.send_message(
+                message.from_user.id,
+                text="Sᴏʀʀʏ, Yᴏᴜ'ʀᴇ Bᴀɴɴᴇᴅ Fʀᴏᴍ Uꜱɪɴɢ Tʜɪꜱ Bᴏᴛ."
+            )  
+    except (UserNotParticipant, KeyError):                       
         return await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
-    return await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
-          
-
-
-
+        
